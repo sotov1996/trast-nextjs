473a6992683729e5891d6dfff53c9fa5fff6c6ec
ilya sotov <sotovilya67@gmail.com>
2021-08-11 09:13:09 +0300

    fix dashboard size brend

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -37,7 +37,7 @@ const Dashboard = ({ brends }) => {
                     position: 'relative',
                     width: '100%',
                     overflow: 'hidden',
-                    minHeight: "259px",
+                    height: "359px",
                     display: "flex",
                     alignItems: "center"
                   }}>
